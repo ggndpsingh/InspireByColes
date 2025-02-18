b55dd587da28eaedbb1d699c7bb0b15e9136579e
Gagandeep Singh <gagan.deep@me.com>
2025-02-18 11:24:33 +1100

    Initial Commit

---
+++ ColesRecipes/ColesRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColesRecipesApp.swift
+//  ColesRecipes
+//
+//  Created by Gagandeep Singh on 18/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColesRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColesRecipesTests/ColesRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColesRecipesTests.swift
+//  ColesRecipesTests
+//
+//  Created by Gagandeep Singh on 18/2/2025.
+//
+
+import Testing
+@testable import ColesRecipes
+
+struct ColesRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
