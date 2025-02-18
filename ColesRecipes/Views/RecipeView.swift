7bb37eead44a6cd16e9df4da158c52def08b57d1
Gagandeep Singh <gagan.deep@me.com>
2025-02-18 18:37:17 +1100

    Create RecipeView

---
+++ ColesRecipes/Views/RecipeView.swift
@@ -0,0 +1,112 @@
+//
+//  RecipeView.swift
+//  ColesRecipes
+//
+
+import SwiftUI
+import ColesInspire
+
+struct RecipeView: View {
+
+    let recipe: Recipe
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 24) {
+                AsyncImageView(url: recipe.thumbnail.url)
+                    .aspectRatio(480/280, contentMode: .fit)
+                    .frame(minHeight: 240)
+
+                VStack(alignment: .leading, spacing: 24) {
+                    titleAndDescription
+
+                    Divider()
+
+                    HStack(spacing: 16) {
+                        durationView(time: recipe.prepTime)
+                        durationView(time: recipe.cookTime)
+                        amountView
+                    }
+                }
+                .padding(.horizontal, 16)
+            }
+        }
+        .ignoresSafeArea(edges: .top)
+    }
+
+    private var titleAndDescription: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack {
+                Capsule().fill(.accent)
+                    .frame(maxHeight: .infinity)
+                    .frame(width: 4)
+
+                Text(recipe.title)
+                    .font(.largeTitle.weight(.bold))
+                    .multilineTextAlignment(.leading)
+            }
+
+            Text(recipe.description)
+        }
+    }
+
+    private var amountView: some View {
+        VStack {
+            Text(recipe.amount.amountType.title)
+                .font(.footnote.weight(.bold))
+            Text(recipe.amount.value, format: .number)
+                .font(.callout)
+        }
+        .pill()
+    }
+
+    private func durationView(time: Recipe.Time) -> some View {
+        VStack(spacing: 0) {
+            Text(time.label)
+                .font(.footnote.weight(.bold))
+
+            Text(
+                time.duration,
+                format: .units(width: .wide, maximumUnitCount: 3)
+            )
+            .font(.callout)
+
+            if let notes = time.note {
+                Text(notes)
+                    .font(.caption)
+            }
+        }
+        .pill()
+    }
+}
+
+struct PillViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(4)
+            .background(.thinMaterial)
+            .clipShape(.capsule)
+            .overlay {
+                Capsule().stroke(.quaternary, lineWidth: 1)
+            }
+    }
+}
+
+extension View {
+    func pill() -> some View {
+        modifier(PillViewModifier())
+    }
+}
+
+extension Recipe.Amount.AmountType {
+    var title: String {
+        switch self {
+        case .serves: "Serves"
+        }
+    }
+}
+
+#Preview {
+    RecipeView(recipe: .mock)
+}
